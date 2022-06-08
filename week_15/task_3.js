8d39b2d07e7350ba4d1c500b760ced98237ce1a6
Dina Popova <studypost1701@gmail.com>
2022-06-09 03:18:32 +0500

    Week 15

--- week_15/task_3.js
+++ week_15/task_3.js
@@ -1,5 +1,14 @@
-const checkBox = document.getElementById('chk');
-
-checkBox.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-});
+let chk = document.getElementById('check');
+chk.addEventListener('change', () => {
+    let mainWrapper = document.getElementById('wrapper');
+    mainWrapper.style.backgroundColor = chk.value;
+    if (chk.value === "#fff6ed") {
+        mainWrapper.style.color = 'black';
+    }
+    else if (chk.value === "#161616") {
+        mainWrapper.style.color = 'white';
+    }
+    else if (chk.value === "#94bcff") {
+        mainWrapper.style.color = 'blue';
+    }
+})
